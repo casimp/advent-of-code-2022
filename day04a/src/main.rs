c3f7835d967ef5748785be3ac7f423e1c30cb5c7
Chris Simpson <chris.simpson@perceptual-robotics.com>
2023-12-09 07:10:05 +0000

    improve structure of day 4

--- day04a/src/main.rs
+++ day04a/src/main.rs
@@ -3,6 +3,40 @@ use std::io::Read;
 use std::path::Path;
 
 
+struct Card {
+    winners: Vec<i32>,
+    selected: Vec<i32>,
+}
+
+impl Card {
+    
+    fn matches(&self) -> Vec<i32>{
+        let mut correct = vec![];
+        for num in &self.selected {
+            if self.winners.contains(&num) {
+                correct.push(*num as i32)
+            }
+        }
+        correct
+    }
+
+    fn points(&self) -> i32{
+        if self.matches().len() > 0 {
+            2i32.pow(self.matches().len().try_into().unwrap()) / 2
+        } else { 0 }
+    }
+
+    fn new(result: &str) -> Self {
+        let (_, game) = result.split_once(": ").unwrap();
+        let (winners_str, select_str) = game.split_once(" | ").unwrap();
+        Self {
+            winners: parse_card_numbers(winners_str), 
+            selected: parse_card_numbers(select_str),
+        }
+    }
+}
+
+
 fn main() {
     let path = Path::new("src/input.txt");
     let display = path.display();
@@ -22,32 +56,18 @@ fn main() {
     let lines: Vec<String> = s.lines().map(String::from).collect();
     let mut total = 0;
 
-    // for each line (game) get the winners and selected values
-    // then check for the intersection of those values
     for line in lines {
-        let mut correct = vec![];
-        let (_, game) = line.split_once(": ").unwrap();
-        let (winners_str, select_str) = game.split_once(" | ").unwrap();
-        let winners: Vec<i32> = winners_str
-            .split_whitespace()
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect();
-        let selected: Vec<i32> = select_str
-            .split_whitespace()
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect();
-        println!("WINNERS: {:?}  OURS: {:?}", winners, selected);
-
-        for num in selected {
-            if winners.contains(&num) {
-                correct.push(num)
-            }
-        }
-        println!("Correct guesses: {:?}", correct);
-        let num_correct: i32 = correct.len() as i32;
-        let points = if num_correct == 0 {0} else {2i32.pow(num_correct.try_into().unwrap()) / 2};
-        println!("Points: {:?}", points);
-        total = total + points;
+        let card = Card::new(&line);
+        println!("Points: {:?}", card.points());
+        total = total + card.points();
     }
     println!("Total: {:?}", total);
+}
+
+fn parse_card_numbers (stringy_numbers: &str) -> Vec<i32> {
+    let numbers: Vec<i32> = stringy_numbers
+        .split_whitespace()
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+    numbers
 }
