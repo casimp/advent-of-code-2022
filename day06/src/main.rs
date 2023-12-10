205d6afeb6dc555db04dd157be1e45ae679d703d
Chris Simpson <chris.simpson@perceptual-robotics.com>
2023-12-10 10:12:37 +0000

    day6b complete and improved 6a

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
-fn parse() -> Vec<Race> {
+fn parse_pt1() -> Vec<Race> {
     let contents =
         fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
     let (times_str, distances_str) = contents.split_once("\n").expect("Should be able to split");
     let times = times_str[10..]
         .split_whitespace()
-        .map(|s| s.parse::<i32>().unwrap());
+        .map(|s| s.parse::<i64>().unwrap());
     let distances = distances_str[10..]
         .split_whitespace()
-        .map(|s| s.parse::<i32>().unwrap());
+        .map(|s| s.parse::<i64>().unwrap());
 
     let races: Vec<Race> = times
         .zip(distances)
@@ -18,50 +18,63 @@ fn parse() -> Vec<Race> {
     races
 }
 
+fn parse_pt2() -> Race {
+    let contents =
+        fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
+    let (times_str, distances_str) = contents.split_once("\n").expect("Should be able to split");
+    let time: i64 = times_str[10..]
+        .split_whitespace()
+        .flat_map(|s| s.chars())
+        .collect::<String>()
+        .parse::<i64>()
+        .unwrap();
+    let record: i64 = distances_str[10..]
+        .split_whitespace()
+        .flat_map(|s| s.chars())
+        .collect::<String>()
+        .parse::<i64>()
+        .unwrap();
+    Race { time, record }
+}
+
 #[derive(Debug)]
 struct Race {
-    time: i32,
-    record: i32,
+    time: i64,
+    record: i64,
 }
 
-struct RaceStrategy {
-    hold: i32,
-    time: i32,
-}
-
-impl RaceStrategy {
-    fn distance(&self) -> i32 {
-        self.hold * (self.time - self.hold)
+impl Race {
+    fn hold_to_break(&self) -> i64 {
+        let a: f64 = -1.0;
+        let b: f64 = self.time as f64;
+        let c: f64 = -self.record as f64;
+        let h: f64 = (-b + (b.powf(2.0) - 4.0 * a * c).powf(0.5)) / (2.0 * a);
+        return h.ceil() as i64;
     }
-    fn breaks_record(&self, record: i32) -> bool {
-        self.distance() > record
+    fn record_breakers(&self) -> i64 {
+        let h = self.hold_to_break();
+        let record_breakers = self.time + 1 - 2 * h;
+        record_breakers
     }
 }
 
 fn pt1() {
-    let races = parse();
+    let races = parse_pt1();
     // println!("{:?}", races);
     let mut breaker_product = 1;
     for race in races {
-        let mut record_breakers = 0;
-        for hold in 0..race.time {
-            let strategy = RaceStrategy {
-                hold,
-                time: race.time,
-            };
-            if strategy.breaks_record(race.record) {
-                record_breakers += 1
-            }
-        }
-        println!("Race breakers: {:?}", record_breakers);
+        let record_breakers = race.record_breakers();
+        println!("Record breakers: {:?}", record_breakers);
         breaker_product *= record_breakers;
     }
-    println!("\nBreaker produce: {:?}", breaker_product);
+    println!("\nBreaker product: {:?}", breaker_product);
 }
 
 fn pt2() {
-    // let contents =
-    //     fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
+    let race = parse_pt2();
+    println!("Race: {:?}\n", race);
+    println!("Broken at {:?}", race.hold_to_break());
+    println!("Record breakers {:?}", race.record_breakers());
 }
 
 fn main() {
